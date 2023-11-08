b500f9046134307ce967f35253f59ef11344004e
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2023-11-08 15:26:21 -0300

    miprimercommit

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
 #from models import Person
-
+#
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
