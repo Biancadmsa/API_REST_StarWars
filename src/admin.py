8f8a8d7423ee35d237a111dd888970331dd5039a
Bianca Salcedo <biancadmsadf16@gmail.com>
2023-11-20 15:17:55 +0000

    miotrocommit

--- src/admin.py
+++ src/admin.py
@@ -1,8 +1,7 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Planets, Characters, Favorite
 from flask_admin.contrib.sqla import ModelView
-
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -12,6 +11,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+    admin.add_view(ModelView(Characters, db.session))
+    admin.add_view(ModelView(Favorite, db.session))
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
